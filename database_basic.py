f6937fd3ac51be42e5080433d0b5c6e33d799dcb
anh <nguyenvieanh2103@gmail.com>
2022-08-06 08:27:48 +0700

    XML

---
+++ database_basic.py
@@ -0,0 +1,13 @@
+"""Xây dựng ứng dụng với csdl
+SQL server, Oracle, MySQL, Postgresql, SQLite
+SQLite là một in-process library, engine không cần server, không cần cấu hình (zero - conflict)
+<400kib
+.....
+Môi bẩng lưu trữ một đói tượng
+table (class) -> field (attribute) -> record (object)
+
+
+
+
+
+"""
