cdf55580e49ffc986fdd8b427f938a5b0fda6165
anh <nguyenvieanh2103@gmail.com>
2022-08-07 10:23:41 +0700

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	db/HangHoa.csv
    #	db/vidu_table1.png

---
+++ thread/my_theard.py
@@ -0,0 +1,38 @@
+import threading
+import time
+
+exitFlag = 0
+
+
+class MyThread(threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+
+    def run(self):
+        print("Starting " + self.name)
+        print_time(threadName=self.name, delay=self.counter, counter=5)
+        print("Exiting " + self.name)
+
+
+def print_time(threadName, delay, counter):
+    while counter:
+        if exitFlag:         
+            threadName.exit()
+            time.sleep(delay)
+        print("%s: %s" % (threadName, time.ctime((time.time()))))
+        counter -= 1
+
+
+
+
+thread1 = MyThread(1, "Thread-1", 1)
+thread2 = MyThread(1, "Thread-2", 2)
+
+thread1.start()
+thread2.start()
+thread1.join()
+thread2.join()
+print("Exiting Main Thread")
